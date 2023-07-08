8f332021432c980ed70e085d5a1a3a27c4218957
TOMOQ1024 <tomo_suzu_16384@au.com>
2023-07-08 09:02:11 +0900

    バナータイプの実装(仮)

--- src/Utils.ts
+++ src/Utils.ts
@@ -31,8 +31,16 @@ export class Expr {
 export interface ParseResult {
   // 正しい構文かどうか
   status: boolean;
+  // 解析によって得られた式の型
+  type: ExprType;
   // glslで実行可能なコードに変換したもの
   result: string;
+}
+
+export const NullParseResult: ParseResult = {
+  status: false,
+  type: 'null',
+  result: ''
 }
 
 export type MouseEventMode = ['set-controls-width'][number]
